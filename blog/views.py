ca5d32976280acc9eff0256d8bed7985a4c73cfd
Fatemeh Amini <fatemehamini.0091@gmail.com>
2024-08-04 13:30:01 +0000

    finish custom filters

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-
+from django.utils import timezone
+from blog.models import Post
 
 def index(request):
-    return render(request, "blog/index.html")
+    posts = Post.objects.filter(published_at__lte=timezone.now())
+    print("aminiiiiii")
+    print(posts)
+    return render(request, "blog/index.html", {"posts":posts})
 
