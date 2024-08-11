1f475a442aed48d9ce34b74dd660fb1a59f9acc6
Fatemeh Amini <fatemehamini.0091@gmail.com>
2024-08-11 11:33:54 +0000

    Finish crispy forms

--- blango/settings.py
+++ blango/settings.py
@@ -51,6 +51,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'crispy_forms',
+    'crispy_bootstrap5',
+
 ]
 """
 Scroll down a bit and look for the definition of the variable MIDDLEWARE. 
@@ -147,3 +150,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
